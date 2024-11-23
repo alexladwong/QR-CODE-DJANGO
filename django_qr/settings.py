d51b515aa74afc6550dd461b15f1c19a9a753cba
AMBASSADOR ALEX LADWONG <81810294+alexladwong@users.noreply.github.com>
2024-11-23 03:06:25 +0300

    Update settings.py

--- django_qr/settings.py
+++ django_qr/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cumpk=+yf0_jr*3&@v!zoo_pfumiowww%pbih$gw69vi&7rkz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
